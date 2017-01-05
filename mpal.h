a622435d6b829e8a38e15e194e22f0437c62ed0e
Pi1eM <291392@uwr.edu.pl>
2017-01-05 10:38:04 +0100

    Podstawa

---
+++ mpal.h
@@ -0,0 +1,17 @@
+#ifndef MPAL
+#define MPAL
+
+#define Hyc {
+#define hyc }
+#define gizmo struct
+#define prosze_panstwa for
+#define i_gotowe return
+#define to_intuicyjnie_jasne main
+#define plUUuus ++
+#define michalki *
+#define pum_pum_pum printf("...");
+
+//Kryakin mode
+#define ³ot_zagwozdka stderr
+
+#endif // MPAL
